75c9ba730c8fcd452c1fffa4667391dcf49d9b49
Tenuxi <jaaltonen86@gmail.com>
2019-08-30 13:19:54 +0300

    else lausetta korjattu

--- db.php
+++ db.php
@@ -14,13 +14,13 @@ if(mysqli_connect_error())
     echo '<div id="huomautus" class="alert alert-danger" role="alert">';
     echo "SQL yhteys ei ole toiminnassa!: " . mysqli_connect_error();
     echo "</div>";
-} else 
+} else {
 
  mysqli_set_charset($connection, 'utf8');
  echo '<div id="sqlToiminnassa" class="alert alert-primary" role="alert">';
  echo "SQL yhteys toimii";
  echo "</div>";
 
-
+}
 
 ?>
